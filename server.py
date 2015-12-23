8984e1a87a0954f0dc0b81908bfe28692fe93e03
Eric Wang <ezwang1@gmail.com>
2015-12-23 09:13:35 -0500

    added track/new functions

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/tracknewpackage')
+def tracknewpackage():
+    name = request.args.get('name')
+    uuid = request.args.get('uuid')
+    destinationLat = request.args.get('destinationLat')
+    destinationLon = request.args.get('destinationLon')
+    return jsonify(**{"ackUUID":"[" + uuid + "]"})
+
+@app.route('/packagetrackupdate/<uuid>', methods=['POST'])
+def packagetrackupdate(uuid):
+    if "delivered" in request.args:
+        pass
+    else:
+        pass
+    return jsonify(**{"ackUUID":"[" + uuid + "]"})
 
 if __name__ == '__main__':
     app.run(threaded=True, port=8080, debug=True)
