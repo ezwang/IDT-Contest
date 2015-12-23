9ad608e394d54f889e23a7d67b22801ca63a815d
Eric Wang <ezwang1@gmail.com>
2015-12-23 12:13:14 -0500

    implemented adding all events to database

--- server.py
+++ server.py
@@ -3,10 +3,14 @@
 import json
 from flask import Flask, request, render_template, jsonify
 
+import psycopg2
+
 app = Flask(__name__, static_url_path='')
 
 with open('config.json') as data:
     config = json.load(data)
+
+conn = psycopg2.connect("dbname='" + config["database"]["dbname"] + "' user='" + config["database"]["user"] + "' host='" + config["database"]["host"] + "' password='" + config["database"]["pass"] + "'")
 
 @app.route('/')
 def root():
@@ -16,16 +20,28 @@ def root():
 def tracknewpackage():
     name = request.args.get('name')
     uuid = request.args.get('uuid')
-    destinationLat = request.args.get('destinationLat')
-    destinationLon = request.args.get('destinationLon')
+    dLat = float(request.args.get('destinationLat'))
+    dLon = float(request.args.get('destinationLon'))
+    cur = conn.cursor()
+    cur.execute('INSERT INTO packages (id, name, destination, delivered) VALUES (%s, %s, \'(%s, %s)\', false)', (uuid, name, dLat, dLon))
+    conn.commit()
     return jsonify(**{"ackUUID":"[" + uuid + "]"})
 
 @app.route('/packagetrackupdate/<uuid>', methods=['POST'])
 def packagetrackupdate(uuid):
     if "delivered" in request.args:
-        pass
+        cur = conn.cursor()
+        cur.execute('UPDATE packages SET delivered = true WHERE uuid = ?', (uuid,))
+        conn.commit()
     else:
-        pass
+        content = request.get_json()
+        lat = float(content['lat'])
+        lon = float(content['lon'])
+        ele = float(content['ele'])
+        time = content['time']
+        cur = conn.cursor()
+        cur.execute('INSERT INTO steps (id, pos, ele, time) VALUES (%s, \'(%s, %s)\', %s, %s)', (uuid, lat, lon, ele, time))
+        conn.commit()
     return jsonify(**{"ackUUID":"[" + uuid + "]"})
 
 if __name__ == '__main__':
--- setup_database.py
+++ setup_database.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
 import psycopg2
+import json
+import sys
 
-# setup database here
+with open('config.json') as data:
+    config = json.load(data)
+
+print "[*] Connecting to database..."
+conn = psycopg2.connect("dbname='" + config["database"]["dbname"] + "' user='" + config["database"]["user"] + "' host='" + config["database"]["host"] + "' password='" + config["database"]["pass"] + "'")
+cur = conn.cursor()
+
+if len(sys.argv) > 1:
+    if sys.argv[1].lower() == "clear":
+        print('[*] Deleting all tables...')
+        cur.execute('DROP TABLE IF EXISTS users, packages, steps')
+        conn.commit()
+
+print "[*] Creating user table..."
+cur.execute('CREATE TABLE IF NOT EXISTS users (id SERIAL, username TEXT, password TEXT, email TEXT, type INTEGER)')
+conn.commit()
+print "[*] Creating package table..."
+cur.execute('CREATE TABLE IF NOT EXISTS packages (id UUID, name TEXT, destination POINT, delivered BOOLEAN)')
+conn.commit()
+print '[*] Creating package steps table...'
+cur.execute('CREATE TABLE IF NOT EXISTS steps (id UUID, pos POINT, ele DOUBLE PRECISION, time TIMESTAMP)')
+conn.commit()
+print '[*] Tasks completed successfully!'
+conn.close()
