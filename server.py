7d165f0112e8b6fbbcf8f874a1e42cee59aa5c34
Eric Wang <ezwang1@gmail.com>
2015-12-23 08:57:39 -0500

    added template system

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=8080, debug=True)
