60c528d37d06d1f5c8fbd5ab5ca24ed71853d84c
Eric Wang <ezwang1@gmail.com>
2015-12-25 18:56:00 -0500

    remove red border when form errors fixed

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("form").submit(function(e) {
         e.preventDefault();
         var valid = true;
-        $(this).find('.form-group').removeClass('has-danger');
+        $(this).find('.input-group').removeClass('has-danger');
         $.each($(this).find('input'), function(k, v) {
             if (!$(this).val()) {
                 valid = false;
