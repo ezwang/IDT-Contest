c259e409aa69fd1341140c49248c13ac9eff6257
Eric Wang <ezwang1@gmail.com>
2015-12-28 17:01:17 -0500

    improved navar

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ conn = psycopg2.connect("dbname='" + config["database"]["dbname"] + "' user='" +
 
 @app.context_processor
 def inject_user_data():
-    return dict(logged_in='id' in session)
+    return dict(logged_in='id' in session, is_admin=session['type'] > 0 if 'type' in session else False)
 
 @app.route('/')
 def root():
